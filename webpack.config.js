71c16eb3dbdf88106408edb1978ed6a1d3fd740d
putchom <putchom8@gmail.com>
2021-09-19 14:02:48 +0900

    sass

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,10 @@ module.exports = (env, argv) => ({
         exclude: /node_modules/
       },
 
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
+      // Enables including Sass by doing "import './file.scss'" in your TypeScript code
       { 
-        test: /\.css$/, 
-        use: ["style-loader", "css-loader"],
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
       // { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] }
