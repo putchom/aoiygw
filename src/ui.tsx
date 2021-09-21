e252e94a7f6074eb031518f15be8ddfc828e45db
putchom <putchom8@gmail.com>
2021-09-22 02:41:41 +0900

    VFC

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,57 +1,50 @@
 import * as React from 'react'
+import { useState } from 'react'
 import * as ReactDOM from 'react-dom'
-import { Button, Text, Title } from 'react-figma-plugin-ds'
+import { Button, Input, Text, Title } from 'react-figma-plugin-ds'
 import './ui.scss'
 
-declare function require(path: string): any
+const App: React.VFC = () => {
+  const [count, setCount] = useState(5)
 
-class App extends React.Component {
-  textbox: HTMLInputElement
-
-  countRef = (element: HTMLInputElement) => {
-    if (element) element.value = '5'
-    this.textbox = element
-  }
-
-  onCreate = () => {
-    const count = parseInt(this.textbox.value, 10)
+  const handleClickCreateButton = () => {
     parent.postMessage({ pluginMessage: { type: 'create-rectangles', count } }, '*')
   }
 
-  onCancel = () => {
+  const handleClickCancelButton = () => {
     parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
   }
 
-  render() {
-    return (
-      <>
-        <Title
-          level='h1'
-          size='large'
-        >
-          Place Aoi Yagawa
-        </Title>
-        <Text
-          weight="medium"
-        >
-          Count:
-        </Text>
-        <input ref={this.countRef} />
-        <Button
-          isPrimary
-          onClick={this.onCreate}
-        >
-          Create
-        </Button>
-        <Button
-          isSecondary
-          onClick={this.onCancel}
-        >
-          Cancel
-        </Button>
-      </>
-    )
-  }
+  return (
+    <>
+      <Title
+        level='h1'
+        size='large'
+      >
+        Place Aoi Yagawa
+      </Title>
+      <Text
+        weight="medium"
+      >
+        Count:
+      </Text>
+      <Input
+        defaultValue={count}
+      />
+      <Button
+        isPrimary
+        onClick={handleClickCreateButton}
+      >
+        Create
+      </Button>
+      <Button
+        isSecondary
+        onClick={handleClickCancelButton}
+      >
+        Cancel
+      </Button>
+    </>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('react-page'))
